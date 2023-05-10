ef86f4bbf70e5bec9fe36dbca5f23bfaaf89d146
nat-davydova <natti.davydova@gmail.com>
2023-05-10 19:10:50 +0300

    feat: very basic layout draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <div className="navigation"></div>
+      <div className="container">
+        <div className="topBar">Top Bar</div>
+        <div className="content"></div>
+        <div className="footer">
+          <p>Footer</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
