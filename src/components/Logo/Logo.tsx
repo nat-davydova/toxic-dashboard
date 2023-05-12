6a7868090f83c0500c31eb6ca74169fc561d4e23
nat-davydova <natti.davydova@gmail.com>
2023-05-12 14:48:52 +0300

    feat: add logo structure and styles

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,5 @@
+import styles from "./Logo.module.css";
+
+export function Logo() {
+  return <span className={styles.logo}>ToxicCity</span>;
+}
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,13 @@
+import { Logo } from "../Logo/Logo.tsx";
+
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <div className={styles.logo}>Logo</div>
+      <div className={styles.logo}>
+        <Logo />
+      </div>
       <div className={styles.avatar}>Avatar</div>
       <div className={styles.userName}>User Name</div>
       <div className={styles.userLvl}>Next User Lvl</div>
