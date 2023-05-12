16e061fb0e11dea5b35945e768e3a6f1c763aa55
nat-davydova <natti.davydova@gmail.com>
2023-05-12 14:38:24 +0300

    feat: add sidebar elems offsets

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,10 +3,10 @@ import styles from "./Sidebar.module.css";
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <div>Logo</div>
-      <div>Avatar</div>
-      <div>User Name</div>
-      <div>Next User Lvl</div>
+      <div className={styles.logo}>Logo</div>
+      <div className={styles.avatar}>Avatar</div>
+      <div className={styles.userName}>User Name</div>
+      <div className={styles.userLvl}>Next User Lvl</div>
       <nav>Navigation</nav>
     </aside>
   );
