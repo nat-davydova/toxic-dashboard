ba0f0f9ceb407b212b07ea723d198d1130e873cc
nat-davydova <natti.davydova@gmail.com>
2023-05-12 14:29:02 +0300

    refactor: outsource sidebar into separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import { Footer } from "./components/Footer/Footer.tsx";
+import { Sidebar } from "./components/Sidebar/Sidebar.tsx";
 
 function App() {
   return (
     <div className="app">
-      <div className="navigation"></div>
+      <Sidebar />
       <div className="container">
         <div className="topBar"></div>
         <div className="content"></div>
