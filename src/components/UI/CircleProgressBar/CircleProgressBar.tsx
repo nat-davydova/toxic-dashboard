f4ae5c8d195dc279be1395402507b80efcd1542f
nat-davydova <natti.davydova@gmail.com>
2023-05-12 16:03:20 +0300

    feat: add circle progress basic svg

--- src/components/UI/CircleProgressBar/CircleProgressBar.tsx
+++ src/components/UI/CircleProgressBar/CircleProgressBar.tsx
@@ -1,3 +1,17 @@
+import styles from "./CircleProgressBar.module.css";
+
 export function CircleProgressBar() {
-  return <div>Progress Bar!</div>;
+  return (
+    <div className={styles.progress}>
+      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <circle
+          cx="50"
+          cy="50"
+          r="42"
+          strokeDasharray="53 150"
+          strokeDashoffset="4"
+        />
+      </svg>
+    </div>
+  );
 }
