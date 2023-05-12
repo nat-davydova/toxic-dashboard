95684ce06eb169919e4081a373dc9f9a19cb30f6
nat-davydova <natti.davydova@gmail.com>
2023-05-12 14:31:52 +0300

    feat: add basic sidebar structure

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,13 @@
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
-  return <aside className={styles.navigation}></aside>;
+  return (
+    <aside className={styles.sidebar}>
+      <div>Logo</div>
+      <div>Avatar</div>
+      <div>User Name</div>
+      <div>Next User Lvl</div>
+      <nav>Navigation</nav>
+    </aside>
+  );
 }
