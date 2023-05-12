8eb3fd8e35b44a0f95beaa68c96bbe73afa8d41d
nat-davydova <natti.davydova@gmail.com>
2023-05-12 16:11:16 +0300

    feat: create circle svg with dynamic parameters (draft)

--- src/components/UI/CircleProgressBar/CircleProgressBar.tsx
+++ src/components/UI/CircleProgressBar/CircleProgressBar.tsx
@@ -1,15 +1,17 @@
 import styles from "./CircleProgressBar.module.css";
 
+const PROGRESS_STROKE_WIDTH_DEFAULT_PX = 3;
+const CIRCLE_CENTER_COORD = 50;
+
 export function CircleProgressBar() {
   return (
     <div className={styles.progress}>
       <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
         <circle
-          cx="50"
-          cy="50"
-          r="42"
-          strokeDasharray="53 150"
-          strokeDashoffset="4"
+          cx={CIRCLE_CENTER_COORD}
+          cy={CIRCLE_CENTER_COORD}
+          r={CIRCLE_CENTER_COORD - PROGRESS_STROKE_WIDTH_DEFAULT_PX}
+          strokeWidth={PROGRESS_STROKE_WIDTH_DEFAULT_PX}
         />
       </svg>
     </div>
