a53f40cbbf74b5e1246d4a8bd2eb03e4740e695f
nat-davydova <natti.davydova@gmail.com>
2023-05-15 17:21:47 +0300

    feat: calc circle radius

--- src/components/UI/CircleProgressBar/CircleProgressBar.tsx
+++ src/components/UI/CircleProgressBar/CircleProgressBar.tsx
@@ -3,14 +3,22 @@ import styles from "./CircleProgressBar.module.css";
 const PROGRESS_STROKE_WIDTH_DEFAULT_PX = 3;
 const CIRCLE_CENTER_COORD = 50;
 
-export function CircleProgressBar() {
+interface ICircleProgressBarProps {
+  strokeWidth?: number;
+}
+
+export function CircleProgressBar({
+  strokeWidth = PROGRESS_STROKE_WIDTH_DEFAULT_PX,
+}: ICircleProgressBarProps) {
+  const radius = CIRCLE_CENTER_COORD - strokeWidth;
+
   return (
     <div className={styles.progress}>
       <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
         <circle
           cx={CIRCLE_CENTER_COORD}
           cy={CIRCLE_CENTER_COORD}
-          r={CIRCLE_CENTER_COORD - PROGRESS_STROKE_WIDTH_DEFAULT_PX}
+          r={radius}
           strokeWidth={PROGRESS_STROKE_WIDTH_DEFAULT_PX}
         />
       </svg>
