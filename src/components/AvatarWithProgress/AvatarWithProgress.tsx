d4b728d66d57ae8a3be613ceeaeb1fc10a7520a8
nat-davydova <natti.davydova@gmail.com>
2023-05-25 20:40:37 +0300

    feat: avatar with progress bar styles

--- src/components/AvatarWithProgress/AvatarWithProgress.tsx
+++ src/components/AvatarWithProgress/AvatarWithProgress.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { Avatar } from "../UI/Avatar/Avatar.tsx";
 import { AvatarSize } from "../UI/Avatar/types.ts";
 import { CircleProgressBar } from "../UI/CircleProgressBar/CircleProgressBar.tsx";
@@ -9,14 +11,28 @@ interface IAvatarWithProgressProps {
   progressPercents: number;
 }
 
+const PROGRESS_STROKE_WIDTH = 3;
+
 export function AvatarWithProgress({
   src,
   progressPercents,
 }: IAvatarWithProgressProps) {
   return (
-    <div className={styles.avatarContainer}>
-      <Avatar size={AvatarSize.STRETCH} imgSrc={src} />
-      <CircleProgressBar progressPercents={progressPercents} />
+    <div className={styles.container}>
+      <div
+        className={styles.avatarContainer}
+        style={
+          {
+            "--stroke-width": `${PROGRESS_STROKE_WIDTH}px`,
+          } as React.CSSProperties
+        }
+      >
+        <Avatar size={AvatarSize.STRETCH} imgSrc={src} />
+      </div>
+      <CircleProgressBar
+        progressPercents={progressPercents}
+        strokeWidth={PROGRESS_STROKE_WIDTH}
+      />
     </div>
   );
 }
