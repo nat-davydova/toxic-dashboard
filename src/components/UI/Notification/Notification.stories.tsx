51e517781129fcdaedc8a91393bf6dfafa5a22bd
nat-davydova <natti.davydova@gmail.com>
2023-06-28 20:56:13 +0300

    feat: support all notification types

--- src/components/UI/Notification/Notification.stories.tsx
+++ src/components/UI/Notification/Notification.stories.tsx
@@ -6,6 +6,14 @@ const meta = {
   title: "Example/Notification",
   component: Notification,
   tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <div style={{ transform: "translateZ(0)", height: "200px" }}>
+        {/* 👇 Decorators in Storybook also accept a function. Replace <Story/> with Story() to enable it  */}
+        <Story />
+      </div>
+    ),
+  ],
 } satisfies Meta<typeof Notification>;
 
 export default meta;
@@ -14,12 +22,30 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
+    type: NotificationType.INFO,
+  },
+};
+
+export const Error: Story = {
+  args: {
     type: NotificationType.ERROR,
   },
+};
 
-  render: (args) => (
-    <div>
-      <Notification {...args} />
-    </div>
-  ),
+export const Info: Story = {
+  args: {
+    type: NotificationType.INFO,
+  },
+};
+
+export const Success: Story = {
+  args: {
+    type: NotificationType.SUCCESS,
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    type: NotificationType.WARNING,
+  },
 };
--- src/components/UI/Notification/Notification.tsx
+++ src/components/UI/Notification/Notification.tsx
@@ -6,6 +6,7 @@ export enum NotificationType {
   SUCCESS = "success",
   WARNING = "warning",
   ERROR = "error",
+  INFO = "info",
 }
 
 interface INotificationProps {
@@ -16,6 +17,9 @@ export function Notification({ type }: INotificationProps) {
   const notificationClassNames = classNames({
     [`${styles.notification}`]: true,
     [`${styles.error}`]: type === NotificationType.ERROR,
+    [`${styles.warning}`]: type === NotificationType.WARNING,
+    [`${styles.success}`]: type === NotificationType.SUCCESS,
+    [`${styles.info}`]: type === NotificationType.INFO,
   });
 
   return <div className={notificationClassNames}>I am notification</div>;
