0705fd9568463b20ad79b66991ebb72517541429
nat-davydova <natti.davydova@gmail.com>
2023-06-28 19:28:12 +0300

    feat: get user data from api and remap it

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { AvatarWithProgress } from "../AvatarWithProgress/AvatarWithProgress.tsx";
 import { Logo } from "../Logo/Logo.tsx";
@@ -6,13 +6,53 @@ import { Logo } from "../Logo/Logo.tsx";
 import styles from "./Sidebar.module.css";
 import avatar from "./assets/avatar.svg";
 
+// Plan:
+// 0.1 Get data from api and map it
+
+// 1. Render Name, Last Name
+// 1.1 refactor
+// 1.2 add notification if no user id
+// 2. Add avatar into API
+// 3. render avatar from API
+
+interface User {
+  id: string;
+  name: string;
+  lastName: string;
+}
+
+function userDataMapper(users: User[]): User | null {
+  const isUserExists = Boolean(users.length);
+
+  if (!isUserExists) {
+    return null;
+  }
+
+  const { id, lastName, name } = users[0];
+
+  if (!id) {
+    throw new Error("User has no id");
+  }
+
+  return {
+    id,
+    name: name ?? "",
+    lastName: lastName ?? "",
+  };
+}
+
 export function Sidebar() {
+  const [user, setUser] = useState<User | null>(null);
+
   useEffect(() => {
     async function getUser() {
-      const user = await fetch(
+      const usersData = await fetch(
         "https://649c561b048075719237fb4b.mockapi.io/user"
       );
-      user.json().then((res) => console.log(res));
+      const users = await usersData.json();
+      const user = userDataMapper(users);
+
+      console.log(user);
     }
 
     getUser();
